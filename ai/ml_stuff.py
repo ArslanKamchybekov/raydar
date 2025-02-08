46326409c7f3d7d9ad69aac34fbf9decba9c83f5
Pranav Shridhar <pranavshridhar0@gmail.com>
2025-02-08 07:10:32 -0600

    Temp change

--- ai/ml_stuff.py
+++
@@ -1,7 +0,0 @@
-import torch
-import torchvision.models as models
-import torch.nn as nn
-from torchvision import transforms
-from PIL import Image
-
-
--- ai/server.py
+++ ai/server.py
@@ -6,52 +6,19 @@ from dotenv import load_dotenv
 from io import BytesIO
 from storage3.exceptions import StorageApiError # type: ignore
 
-import torch
-import torchvision.models as models
-import torch.nn as nn
-from torchvision import transforms
-from PIL import Image
-
 from text_extraction.main import predict_the_description_main
 
 load_dotenv()
 storage = supabase.storage
 bucket_name = "lost_images"
 
-# MODEL INIT
-# FIXME: This is giving warnings. Fix it before submission.
-#########################
-class_labels_path = 'classes.txt'  
-with open(class_labels_path, 'r') as file:
-    class_labels = [line.strip() for line in file.readlines()]
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-model = models.resnet18(pretrained=False)  # Don't load the pre-trained weights
-model.fc = nn.Linear(model.fc.in_features, 250)  # Set to 250 classes (based on trained model)
-model.load_state_dict(torch.load('resnet18_trained_model.pth'))
-model = model.to(device)  # Move to GPU if available
-model.eval()  # Set to evaluation mode (important!)
-transform = transforms.Compose([
-    transforms.Resize((150, 150)),  # Resize to 150x150
-    transforms.ToTensor(),  # Convert the image to a tensor
-    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])  # Normalize
-])
+
 
 
 
 app = Flask(__name__)
 CORS(app)  # Enable CORS for all routes
 
-def predict_the_image(image_data):
-    image = Image.open(BytesIO(image_data))
-    image = transform(image).unsqueeze(0)
-    image = image.to(device)
-
-    with torch.no_grad():  # No need to calculate gradients during inference
-        outputs = model(image)  # Get raw outputs (logits)
-        # Get the predicted class (the one with the highest score)
-        _, predicted_class = torch.max(outputs, 1)
-
-    return class_labels[predicted_class.item()]
 
 
 def predict_the_description(input_description, threshold=0.45):
@@ -96,7 +63,8 @@ def get_images():
             print(f"Unexpected error: {str(e)}")
             break
 
-    label = predict_the_image(image_data)
+    
+    label = predict_the_image(image)
     print(f"Predicted keyword: {label}")
 
 
