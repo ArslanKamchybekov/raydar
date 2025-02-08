38ab6d69b3d6a04ad0875ac0e2ef6109c7fcae7b
unknown <anirudhkuppili@gmail.com>
2025-02-07 21:00:43 -0600

    Altered database.

--- constants/brands.ts
+++ constants/brands.ts
@@ -11,4 +11,6 @@ export const brands = [
   "jbl",
   "beats",
   "asus",
+  "other",
+  "oikos",
 ];
--- constants/materials.ts
+++ constants/materials.ts
@@ -9,4 +9,6 @@ export const materials = [
   "rubber",
   "composite",
   "synthetic",
+  "unknown",
+  "edible",
 ];
