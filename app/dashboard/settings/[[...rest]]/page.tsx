51ab8ffd56d2e01fb30f414e607b993d9c5cf04e
Arslan Kamchybekov <arslankamcybekov7@gmail.com>
2025-02-08 08:08:25 -0600

    fixed theme

--- app/dashboard/settings/[[...rest]]/page.tsx
+++ app/dashboard/settings/[[...rest]]/page.tsx
@@ -1,13 +1,19 @@
+"use client";
+
 import { UserProfile } from "@clerk/nextjs";
 import { dark } from "@clerk/themes";
+import { useTheme } from "next-themes";
 
 export default function Settings() {
+  const { resolvedTheme } = useTheme();
+
   return (
-    <div className='flex justify-start items-center flex-wrap px-4 pt-5 gap-4'>
-        <h2 className="mt-10 scroll-m-20 border-b pb-2 w-full text-3xl font-semibold tracking-tight transition-colors first:mt-0">
-          My Profile
-        </h2>
-        <UserProfile appearance={{ baseTheme: dark }} />
+    <div className="flex justify-start items-center flex-wrap px-4 pt-5 gap-4">
+      <h2 className="mt-10 scroll-m-20 border-b pb-2 w-full text-3xl font-semibold tracking-tight transition-colors first:mt-0">
+        My Profile
+      </h2>
+
+      <UserProfile appearance={{ baseTheme: resolvedTheme === "dark" ? dark : undefined }} />
     </div>
-  )
+  );
 }
