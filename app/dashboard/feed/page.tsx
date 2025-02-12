f402d26ab851cb42596e7931c3b505806200a1fd
Arslan Kamchybekov <arslankamcybekov7@gmail.com>
2025-02-12 11:11:03 -0600

    prod

--- app/dashboard/feed/page.tsx
+++ app/dashboard/feed/page.tsx
@@ -55,7 +55,7 @@ const FeedPage = () => {
   return (
     <>
       <NavBar />
-      <div className="container mx-auto px-4 py-8">
+      <div>
         <div className="space-y-4">
           <h1 className="text-2xl md:text-3xl font-semibold tracking-tight mb-2">
             Feed
